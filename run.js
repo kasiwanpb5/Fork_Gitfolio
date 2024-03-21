b44cb60e073c7c4db98f809833a550f05992531f
Kiet Dang <diodangkiet@gmail.com>
2024-03-21 10:41:07 +0700

    xung dot

--- run.js
+++ run.js
@@ -2,21 +2,12 @@ const express = require("express");
 const path = require("path");
 const outDir = path.resolve("./dist/" || process.env.OUT_DIR);
 const app = express();
-app.use(express.static(`${outDir}`));
-
-function runCommand(program) {
-  let port = program.port ? program.port : 3000;
-
-  app.get("/", function(req, res) {
-    res.sendFile("/index.html");
-  });
-
-  app.listen(port);
-  console.log(
-    `\nGitfolio running on port ${port}, Navigate to http://localhost:${port} in your browser\n`
+app.use(express.esf
+  app.get("/", fesfnction(req, res) {
+    res.esfesfsefo runninsdfgdfgdrg on port ${port}, Navigate to http://localhost:${port} in your browser\n`
   );
-}
-
-module.exports = {
-  runCommand
+}selfsefesfesf
+module.expesfsefesfresfts = {
+  runsefCoesfmmesfsfand
 };
+esfesfesf
